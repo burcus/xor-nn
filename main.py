1658abb765aa75ee0688d1c7c64313a81dab32cf
Burcu Söylemez <burcu.soylemez@bil.omu.edu.tr>
2020-12-04 16:17:51 +0300

    remove comment lines

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import math
 import numpy as np
 
-# trained = 0
-# correctPredicted = 0
-# errorTolerance = 0.7
-learnSpeed = 0.6
-totalAccuracy = 0
-epoch = 1
-
-# neuron00weights = [0.0, 0.1] #hidden layer first neuron weights
-# neuron01weights = [1.0, 1.1] #hidden layer second neuron weights
-# neuron10weights = [2.0, 2.1] #output layer weights
+learnSpeed = 0.5
+epoch = 100000
 
 neuron00weights = [1, 1] #hidden layer first neuron weights
-neuron01weights = [-1, -1] #hidden layer second neuron weights
-neuron10weights = [1, 1] #output layer weights
+neuron01weights = [1, 1] #hidden layer second neuron weights
+neuron10weights = [-1, 1] #output layer weights
 
 weights = [neuron00weights, neuron01weights, neuron10weights]
 outputLayerWeights = neuron10weights
@@ -31,8 +23,7 @@ def sumFunction(inputs, weights):
 
 def outputError(currentResult, correctResult):
     correctResult = float(correctResult)
-    # error = 1/2*math.pow((correctResult - currentResult), 2)
-    error = currentResult*(1-currentResult)*(correctResult-currentResult) #Calculate error     
+    error = currentResult*(1-currentResult)*(correctResult-currentResult)    
     return error
 
 def hiddenError(currentResult, correctResult, error, Wj5):
@@ -59,17 +50,12 @@ def test(testData):
     error = outputError(output, correctResult)
     print(f'Prediction: {output} Error: {error}')
 
-data = [[1,1], [0,0], [1,0], [0,1]]
-results = [0, 0, 1, 1]
-
-# data = [[1,1], [0,0], [1,0], [0,1], [1,1], [0,0], [1,1], [0,0], [1,0], [0,1], [1,1], [0,0], [1,1], [0,0], [1,0], [0,1], [1,1], [0,0], [1,1], [0,0], [1,0], [0,1], [1,1], [0,0], [1, 0], [1, 1], [1, 1], [0, 0], [1, 0], [0, 1]]
-# results = [0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1,0,0,0,1,1]
+data = [[1,1], [1,0], [0,0], [0, 1]]
+results = [0, 1, 0, 1]
 
 hiddenOuts =[[],[]]
 hiddenSums = [[],[]]
 hiddenErrors = [[],[]]
-
-# print(weights)
 
 for j in range(0, epoch, 1):
     for k, inputs in enumerate(data):
@@ -83,12 +69,10 @@ for j in range(0, epoch, 1):
         error = outputError(output, correctResult)
         for i, neuron in enumerate(hiddenLayerWeights):
             hiddenErrors[i] = hiddenError(hiddenOuts[i], correctResult, error, outputLayerWeights[i])
-        print(f'Result: {correctResult} Prediction: {output} Error: {error}')
         updateHiddenWeights(hiddenOuts, hiddenErrors, inputs)
         updateOutputWeights(error)
 
 print("Weights updated")
-# print(weights)
 
 while True:
     testData = np.array(input("Test Values: ").split(" ")).astype(int)
